41c7865098f02b902b0a864dd1ffc77f0ea456a3
kimera_han <kimerahan2@gmail.com>
2017-07-24 15:26:23 +0300

    first commit

---
+++ SampleDatabase.java
@@ -0,0 +1,14 @@
+package com.a23labs.room;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by pluto on 7/22/17.
+ */
+/*@Database(entities = {University.class}, version = 1)*/
+@Database(entities = {Song_Container.class,University.class}, version = 1)
+    public abstract class SampleDatabase extends RoomDatabase {
+        public abstract DaoAccess daoAccess();
+    }
+
